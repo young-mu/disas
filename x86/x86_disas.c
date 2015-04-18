1f543a140804e7421e135cf92ed4e49a02c08fe5
young <young.mwy@gmail.com>
2015-04-18 16:09:56 +0800

    support bincode

--- x86/x86_disas.c
+++ x86/x86_disas.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
+#include <assert.h>
 #include <sys/stat.h>
 
 #define X86_STUB_OFFSET     (0x3e0+0x3)
 
-#define NELEM(x)    ((int)(sizeof(x) / sizeof(x[0])))
+void fillCode(unsigned char *dst, const char *src, int size)
+{
+    int i, h, l;
+    for (i = 0; i < size; i++) {
+        if (isxdigit(src[0]) && isxdigit(src[1])) {
+            h = isdigit(src[0]) ? (src[0] - '0') : (src[0] - 'a' + 10);
+            l = isdigit(src[1]) ? (src[1] - '0') : (src[1] - 'a' + 10);
+            *dst++ = h * 16 + l;
+            src += 2;
+        } else {
+            fprintf(stderr, "%c%c is NOT hex!\n", src[0], src[1]);
+            exit(1);
+        }
+    }
+}
 
 int getFileSize(const char *path)
 {
@@ -22,20 +38,45 @@ int main(int argc, const char *argv[])
     int fd_src, fd_stub;
     int code_sz;
     int read_sz, write_sz;
+    char filepath[100];
     int cn;
-    char *code = NULL;
+    unsigned char *code = NULL;
 
     if (argc == 1) {
-        fprintf(stderr, "Usage: ./x86_disas FILE\n");
+        fprintf(stderr, "Usage:\n1) ./x86_disas f <file>\n2) ./x86_disas <bincode>\n");
         exit(1);
-    } else if (argc == 2) {
-        /* get code size */
-        code_sz = getFileSize(argv[1]);
-        if (code_sz == -1) {
-            fprintf(stderr, "fail to getFileSize\n");
-            exit(2);
+    } else if (argc == 2 || argc == 3) {
+
+        if (argc == 2) { /* bincode */
+            assert(strlen(argv[1]) % 2 == 0);
+            code_sz = strlen(argv[1]) >> 1;
+            code = (unsigned char*)malloc(code_sz);
+            fillCode(code, argv[1], code_sz);
+        } else if (argc == 3 && *argv[1] == 'f') { /* binfile */
+            strcpy(filepath, argv[2]);
+
+            /* get code size */
+            code_sz = getFileSize(filepath);
+            if (code_sz == -1) {
+                fprintf(stderr, "fail to getFileSize\n");
+                exit(1);
+            }
+            code = (unsigned char*)malloc(code_sz);
+
+            /* read code */
+            fd_src = open(filepath, O_RDONLY);
+            if (fd_src == -1) {
+                fprintf(stderr, "fail to open %s\n", filepath);
+                exit(1);
+            }
+            read_sz = read(fd_src, code, code_sz);
+            if (read_sz != code_sz) {
+                fprintf(stderr, "fail to read %s\n", filepath);
+                exit(1);
+            }
+
+            close(fd_src);
         }
-        code = (char*)malloc(code_sz);
 
         /* generate x86_stub */
         system("sed -i '/asm/d' ./stub/x86_stub.c");
@@ -44,39 +85,28 @@ int main(int argc, const char *argv[])
         }
         system("gcc ./stub/x86_stub.c -m32 -o ./x86_stub");
 
-        /* read code */
-        fd_src = open(argv[1], O_RDONLY);
-        if (fd_src == -1) {
-            fprintf(stderr, "fail to open %s\n", argv[1]);
-            exit(3);
-        }
-        read_sz = read(fd_src, code, code_sz);
-        if (read_sz != code_sz) {
-            fprintf(stderr, "fail to read %s\n", argv[1]);
-            exit(4);
-        }
-
         /* patch code into stub */
         fd_stub = open("./x86_stub", O_RDWR, 0);
         if (fd_stub == -1) {
             fprintf(stderr, "fail to open x86_stub\n");
-            exit(5);
+            exit(1);
         }
         lseek(fd_stub, X86_STUB_OFFSET, SEEK_SET);
         write_sz = write(fd_stub, code, code_sz);
         if (write_sz != code_sz) {
             fprintf(stderr, "fail to write x86_stub\n");
-            exit(6);
+            exit(1);
         }
 
         free(code);
-        close(fd_src);
         close(fd_stub);
     }
 
+    /* generate assembly */
     system("objdump -d x86_stub \
            | sed -n '/<x86_stub>:/,/<main>:/{/<x86_stub>:/n; /^$/b; /<main>:/d; s/^ /0x/g; p}' \
            | sed -n '3,$p' | head -n -2 \
            | awk --non-decimal-data 'BEGIN {FS=\":\"} {$1 = $1 - 0x80483e3; print $0}'");
+
     return 0;
 }
